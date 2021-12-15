7d97f4ee0e83b1ebb3b2bbf4d1f229f04d9b4d16
caleman9791 <caleman9791@protonmail.com>
2021-12-14 22:38:13 -0400

    Contenu=ido nuevo

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>Document</title>
-</head>
-<body>
-
-</body>
+  <head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <h4>
+      Cambio nuevo
+    </h4>
+  </body>
 </html>
