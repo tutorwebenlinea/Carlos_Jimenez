144e405dd39cca9242898b7188c90de06bcbdef9
caleman9791 <caleman9791@protonmail.com>
2021-12-14 23:50:34 -0400

    Directorio de inicio y algo más. Es poco, pero será modificado en gran manera durante el  proceso de desarrollo.

--- htdocs/index.php
+++ htdocs/index.php
@@ -13,5 +13,10 @@
 
       Lorem ipsum dolor sit amet consectetur adipisicing, elit. Fugit assumenda ex molestias error illo aliquam dignissimos aliquid architecto, modi officia animi dolorem esse. Facilis facere, cumque doloribus est aspernatur illo!
     </h4>
+
+    <p>
+    	<!--  Incluimos el fichero ubicado en le directorio PHP con la instrucción de turno -->
+    	<?php include "./php/detecta_qr.php";?>
+    </p>
   </body>
 </html>
