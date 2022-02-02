ec7f6b86f7c9958764f69eabf12804aaa52668ec
Carlos Aleman <caleman9791@gmail.com>
2022-02-01 22:25:30 -0400

    Ejemplo de conexión

--- htdocs/sql/sql.sql
+++ htdocs/sql/sql.sql
@@ -128,13 +128,13 @@ CREATE TABLE id_propiedad_criterio(
 
 -- Nombre del edificio comenterios del piso y nombre del cubiculo 
 
-SELECT edificio.nombre_edicicio,piso.comentarios, cubiculo.nombre_cubiculo
-FROM edificio
-    INNER JOIN piso
-        ON piso.id_edificio = edificio.id_edificio
-    INNER JOIN cubiculo
-        ON cubiculo.id_piso = piso.id_piso
-WHERE cubiculo.id_cubiculo=2;
+-- SELECT edificio.nombre_edicicio,piso.comentarios, cubiculo.nombre_cubiculo
+-- FROM edificio
+--     INNER JOIN piso
+--         ON piso.id_edificio = edificio.id_edificio
+--     INNER JOIN cubiculo
+--         ON cubiculo.id_piso = piso.id_piso
+-- WHERE cubiculo.id_cubiculo=2;
 
 CREATE TABLE Persons (
     Personid int NOT NULL AUTO_INCREMENT,
